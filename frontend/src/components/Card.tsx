a8b92ed0908a6e234fb4e19d84e1314fb5be6a78
abdelsterdam <abdelelmour@gmail.com>
2025-08-11 01:21:58 +0200

    Delete frontend directory

--- frontend/src/components/Card.tsx
+++
@@ -1,5 +0,0 @@
-
-import { ReactNode } from 'react'
-export default function Card({children}:{children:ReactNode}){
-  return <div className="bg-zinc-900 rounded-2xl p-4 border border-zinc-800">{children}</div>
-}
